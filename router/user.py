261fcd9d8ed9fe2cacc2cb87392a217162ae6559
jtabordave <jtaborda@gmail.com>
2023-09-11 05:06:00 -0500

    Ajustes apis

--- router/user.py
+++ router/user.py
@@ -211,6 +211,11 @@ def login(login: Login = Body(...)):
             )
     # Check if password doesn't exist
     data = query.fetchone()
+    if data.role_id != 1:
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail = "No tiene acceso a este portal."
+            )
     if not verify_password(password, data.password):
         raise HTTPException(
             status_code = status.HTTP_400_BAD_REQUEST,
